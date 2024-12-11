3dda91082f403ede6b42df8a3016f569ac80753e
seunosinowo <oluwaseunpaul98@gmail.com>
2024-12-11 06:09:41 +0100

    Implemented the Food Section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import logo from "/assets/images/freshcrate_logo.png";
 import { company, support } from '../constants/navLinks';
 import { Facebook, Instagram, Twitter, Linkedin } from 'lucide-react';
