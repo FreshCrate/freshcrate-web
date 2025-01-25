ee9cef623bdb5af30f2a82ad2f342dec3ca7b17a
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2025-01-25 06:01:29 +0100

    feat: Changed to Codebase to TS

---
+++ freshcrate-web/src/App.tsx
@@ -0,0 +1,16 @@
+import Landing from "./components/Landing";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Landing />
+      <About />
+    </>
+  );
+}
+
+export default App;
