677e2b5d43bade664756ce026f5d55bfc70b22b7
seunosinowo <oluwaseunpaul98@gmail.com>
2025-02-18 15:28:19 +0100

    Added header bar

--- src/components/Faqs.tsx
+++ src/components/Faqs.tsx
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet";
+import faq_header from "/assets/images/faq_header.png";
 
 const FAQ = () => {
   return (
@@ -9,6 +10,10 @@ const FAQ = () => {
       </Helmet>
       <h1 className="py-8 text-4xl text-center">Frequently Asked Questions</h1>
       {/* Rest of the FAQ content */}
+      <img src={faq_header} alt="FAQ Icon" 
+      className="w-9/12 mx-auto md:pt-9 pt-7" />
+
+      Faq
     </div>
   );
 };
